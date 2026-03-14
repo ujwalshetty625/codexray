39221149b12881f3c0c756d90773fb985b37e54a
Ujwal <ujwalshetty625@gmail.com>
2026-03-14 10:41:57 +0530

    fix path alias for vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next"
+import path from "path"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    }
+    return config
+  },
+}
 
 export default nextConfig
